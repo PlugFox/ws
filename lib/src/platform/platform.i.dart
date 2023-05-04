c5ecd4fbbe1ebb382dd1ce24ef630592ea0761c7
Plague Fox <plugfox@gmail.com>
2023-05-05 00:09:32 +0400

    Update comments

--- lib/src/platform/platform.i.dart
+++ lib/src/platform/platform.i.dart
@@ -79,6 +79,8 @@ enum WebSocketReadyState {
   String toString() => name;
 }
 
+/// Crossplatform binding to the native WebSocket.
+///
 /// The WebSocket object provides the API for creating and managing
 /// a WebSocket connection to a server, as well as for sending
 /// and receiving data on the connection.
